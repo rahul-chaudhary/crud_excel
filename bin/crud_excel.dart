0463da8603a4cf44c29d006288063682b6d3c490
rahulchaudhary <rahul.chaudhary@autolflowtech.com>
2025-06-30 15:27:46 +0530

    Refactor main function to generate locale JSON files from Excel localization sheet

--- bin/crud_excel.dart
+++ bin/crud_excel.dart
@@ -1,20 +1,35 @@
 import 'dart:io';
+import 'package:crud_excel/build_locales.dart';
 import 'package:crud_excel/crud_excel.dart';
 import 'package:excel/excel.dart';
 
-void main() {
-  final bytes = File('assets/localizations.xlsx').readAsBytesSync();
-  final excel = Excel.decodeBytes(bytes);
 
-  final dupes = findDuplicateCellsInColumn(
-    excel: excel,
-    sheetName: 'others',
-    column: 0, // Column A
-  );
-
-  if (dupes.isEmpty) {
-    print('No duplicates found.');
-  } else {
-    print('Duplicate cells: ${dupes.join(", ")}');
+void main() async {
+  try {
+    await buildLocaleJsons(
+      excelPath: 'assets/localizations.xlsx',
+      outputDir: 'assets/localizations',
+      sheetIndex: 1
+    );
+    print('Locale JSON files generated successfully.');
+  } catch (e) {
+    print('Error: $e');
   }
 }
+
+// void main() {
+//   final bytes = File('assets/localizations.xlsx').readAsBytesSync();
+//   final excel = Excel.decodeBytes(bytes);
+
+//   final dupes = findDuplicateCellsInColumn(
+//     excel: excel,
+//     sheetName: 'others',
+//     column: 0, // Column A
+//   );
+
+//   if (dupes.isEmpty) {
+//     print('No duplicates found.');
+//   } else {
+//     print('Duplicate cells: ${dupes.join(", ")}');
+//   }
+// }
